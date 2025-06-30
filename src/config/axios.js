ca0fcb68d4cc34043535889c0c36d89182adb1e6
Wat2705 <vuvtvip102@gmail.com>
2025-07-01 02:44:07 +0700

    config axios interceptor

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,48 @@
-import axios from "axios";
-const { VITE_BASE_URL } = import.meta.env
+import axios from 'axios';
+const { VITE_BASE_URL } = import.meta.env;
 
-axios.defaults.baseURL = VITE_BASE_URL
-axios.defaults.headers['Content-Type'] = 'application/json'
+const axiosInstance = axios.create({
+  baseURL: VITE_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    console.log('Request config:', config);
+    return config;
+  },
+  (error) => {
+    console.error('Request error:', error);
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    console.log('Response:', response.data);
+    return response;
+  },
+  (error) => {
+    console.error('Response error:', {
+      status: error.response?.status,
+      data: error.response?.data,
+      message: error.message,
+    });
+
+    if (error.response && error.response.status === 401) {
+      console.log('Unauthorized, logging out...');
+      localStorage.removeItem('token');
+      window.location.href = '/login';
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
