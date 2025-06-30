3915a510e44b2aae4f7cea42a05b97679f08418c
Wat2705 <vuvtvip102@gmail.com>
2025-07-01 02:44:44 +0700

    add logout action

--- src/redux/auth/index.js
+++ src/redux/auth/index.js
@@ -1,23 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    isLogIn: false,
-    user: null,
-    id: ''
+  isLogIn: false,
+  user: null,
+  id: '',
 };
 
 export const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    reducers: {
-        loginSuccess: (state, action) => {
-            state.isLogIn = true;
-            state.user = action.payload.user;
-            state.id = action.payload.id
-            localStorage.setItem('token', action.payload.token);
-        },
+  name: "auth",
+  initialState,
+  reducers: {
+    loginSuccess: (state, action) => {
+      state.isLogIn = true;
+      state.user = action.payload.user;
+      state.id = action.payload.id;
+      localStorage.setItem('token', action.payload.token);
     },
+    logout: (state) => {
+      state.isLogIn = false;
+      state.user = null;
+      state.id = '';
+      localStorage.clear();
+    },
+  },
 });
 
-export const { loginSuccess } = authSlice.actions;
+export const { loginSuccess, logout } = authSlice.actions;
 export default authSlice.reducer;
